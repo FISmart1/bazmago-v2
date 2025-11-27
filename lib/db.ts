88b3b466a0c89c80a93107409babc1210a8430d7
FISmart1 <yusufftibazma@gmail.com>
2025-11-27 11:37:07 +0700

    edit database

--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,20 @@
-// src/lib/db.ts
 import mysql from 'mysql2/promise';
 
-// buat koneksi pool ke MySQL lokal
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'ucup',           // ganti sesuai user MySQL kamu
-  password: '12345',           // isi kalau ada password
-  database: 'bazmago', // ganti nama DB kamu
+  host: 'nozomi.proxy.rlwy.net',
+  port: 10360,
+  user: 'root',
+  password: 'TpQaGRDedFRJQOvUCRMfKikAHWynQVUX',
+  database: 'railway',
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export async function query<T = any>(sql: string, params?: any[]): Promise<T[]> {
   try {
     const [rows] = await pool.query(sql, params);
-return rows as T[];
-
+    return rows as T[];
   } catch (err) {
     console.error('Database query error:', err);
     throw err;
